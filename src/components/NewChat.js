f368ac7c6cd909104c4d5e1a1b6ba8a6515ba49f
anjalisin <104627622+anjalisin@users.noreply.github.com>
2025-09-14 22:58:23 +0530

    Add files via upload

--- src/components/NewChat.js
+++ src/components/NewChat.js
@@ -1,1 +1,11 @@
+import React from "react";
+import { Plus } from "lucide-react";
 
+export default function NewChat() {
+  return (
+    <button className="flex items-center w-full p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+      <Plus className="mr-2" size={16} />
+      New Chat
+    </button>
+  );
+}
