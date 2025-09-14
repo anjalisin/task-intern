9517a7a571f126d8acff4915ac03b5111a3eb161
anjalisin <104627622+anjalisin@users.noreply.github.com>
2025-09-14 22:52:01 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
+import React from "react";
+import ChatList from "./components/ChatList";
+import ChatWindow from "./components/ChatWindow";
+import NewChat from "./components/NewChat";
 
+export default function App() {
+  return (
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <div className="w-1/4 border-r p-4">
+        <NewChat />
+        <ChatList />
+      </div>
+
+      {/* Chat Window */}
+      <div className="flex-1">
+        <ChatWindow />
+      </div>
+    </div>
+  );
+}
